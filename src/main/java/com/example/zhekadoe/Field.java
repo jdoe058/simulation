b88321c8ed95b6cafd36cda755fce80fad7ed9d1
Zheka Doe <jdoe058@gmail.com>
2024-11-05 21:42:08 +0300

    add getNeighboringCells method

--- src/main/java/com/example/zhekadoe/Field.java
+++ src/main/java/com/example/zhekadoe/Field.java
@@ -2,9 +2,7 @@ package com.example.zhekadoe;
 
 import com.example.zhekadoe.entities.Entity;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * Представляет поле симуляции, является набором ячеек с координатами от левого верхнего угла
@@ -23,6 +21,18 @@ public class Field {
 
     public int getEmptyCellCount() {
         return size - fields.size();
+    }
+
+    Set<Cell> getNeighboringCells(Cell c) {
+        HashSet<Cell> cells = new HashSet<>();
+        for (int i = Math.max(0, c.y() - 1); i < Math.min(height, c.y() + 2); i++) {
+            for (int j = Math.max(0, c.x() - 1); j < Math.min(width, c.x() + 2); j++) {
+                if (i != c.y() || j != c.x()) {
+                    cells.add(new Cell(j, i));
+                }
+            }
+        }
+        return cells;
     }
 
     public Optional<Entity> get(Cell c) {
